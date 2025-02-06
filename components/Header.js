105e6d6664a898da4ab7d5cdfcb4ad4a0d183c7a
IvanLeffalle <ivanmleffalle@gmail.com>
2025-02-06 10:37:54 -0300

    homepage

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import { createElement } from "../src/utils";
+
+export default function Header() {
+  const logo = createElement("div", {
+    className: "logo",
+    textContent: "FamilyHub",
+  });
+
+  const aHome = createElement("a", { href: "/", textContent: "Home" });
+  const aTools = createElement("a", { href: "/links", textContent: "Tools" });
+  const aAbout = createElement("a", { href: "/about", textContent: "About" });
+
+  const li = createElement("li", {}, [aHome, aTools, aAbout]);
+
+  const ul = createElement("ul", {}, [li]);
+
+  const nav = createElement("nav", { className: "nav" }, [ul]);
+  const section = createElement("section", { className: "header-container" }, [
+    logo,
+    nav,
+  ]);
+
+  return createElement("div", { className: "" }, [section]);
+}
--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import { createElement } from "./utils";
+import Homepage from "../pages/HomePage";
 
 export default function App() {
-  const header = createElement("h1", {
-    textContent: "GeneasssssHub",
-    className: "heading",
-  });
-
-  const main = createElement("main");
-
-  return createElement("div", {}, [header, main]);
+  return createElement("div", {}, [Homepage()]);
 }
